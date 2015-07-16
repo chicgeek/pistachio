41342af9b98e024a7b792caca27a224f5265c144
Stuart Wilson <stuart.wilson@graze.com>
2015-07-16 16:37:15 +0100

    Tidy build process and add profile task

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,80 @@
 var gulp = require('gulp');
-var less = require('gulp-less-sourcemap');
+var log = require('./lib/log');
+var compileLess = require('./lib/compile-less');
+var publishDirectory = require('./lib/publish-directory');
+var stylestats = require('gulp-stylestats');
 
-var buildLess = function(cb) {
-    gulp.src('./less/pistachio.less')
-    .pipe(less())
-    .on('error', function(err) {
-        console.log(err.message);
-    })
-    .pipe(gulp.dest('./css'))
-    .on('end', function() {
-        console.log('compiled CSS');
-
-        gulp.src('./css/**/*', {base: './'})
-        .pipe(gulp.dest('./dev/public'))
-        .on('end', function() {
-            if (cb) {
-                cb();
-            }
-        });
-    });
-}
-
+// Watch tasks
 gulp.task('watch', ['watch:less']);
 
+// Watch LESS dir and build to ./css
+// Then publish CSS
 gulp.task('watch:less', function(cb) {
+    var compile = function() {
+        compileLess({
+            file: './less/pistachio.less',
+            dest: './css',
+            done: function() {
+                publishDirectory({
+                    dir: './css/**/*',
+                    dest: './dev/public'
+                });
+            }
+        });
+    }
+
+    compile();
+
     gulp.watch('./less/**/*.less', function() {
-        buildLess();
+        compile();
     });
 });
 
+// Build tasks
 gulp.task('build', ['build:less']);
 
+// Build CSS from LESS
 gulp.task('build:less', function(cb) {
-    buildLess(function() {
-        cb();
+    compileLess({
+        file: './less/pistachio.less',
+        dest: './css',
+        done: cb
     });
 });
 
-gulp.task('dev', ['dev:publish']);
+// Misc dev tasks
+gulp.task('dev', ['dev:publish', 'dev:profile']);
 
+// Publish static files to docs
 gulp.task('dev:publish', function(cb) {
-    gulp.src('./css/**/*', {base: './'})
-    .pipe(gulp.dest('./dev/public'))
-    .on('end', function() {
-        console.log('Published CSS to dev site!');
-    });
+    publishDirectory({
+        dir: './css/**/*',
+        dest: './dev/public',
+        done: function() {
+            log.success('Published CSS to docs');
 
-    gulp.src('./fonts/**/*', {base: './'})
-    .pipe(gulp.dest('./dev/public'))
-    .on('end', function() {
-        console.log('Published fonts to dev site!');
+            publishDirectory({
+                dir: './fonts/**/*',
+                dest: './dev/public',
+                done: function() {
+                    log.success('Published fonts to docs');
+                    cb();
+                }
+            });
+        }
+    });
+});
+
+gulp.task('dev:profile', function(cb) {
+    compileLess({
+        file: './less/pistachio.css',
+        dest: './css',
+        done: function() {
+            gulp.src('./css/pistachio.css')
+            .pipe(stylestats())
+            .on('end', function() {
+                log.info('Profiling complete')
+            })
+        }
     });
 });
